447766c47bb22b7fd207fab543ffbc5a67fec377
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-07-17 18:59:34 -0400

    Added login/sign up logic

--- Password_Manager/src/main.py
+++ Password_Manager/src/main.py
@@ -2,14 +2,27 @@ import accounts
 import sqlite3
 import tkinter
 
+def createUser(loginInfo: dict, user: str, password: str) -> None:
+    loginInfo['users'].append(user)
+    loginInfo['encrypted-passwords'].append(password)   #this has to be encrypted before appending
+
 def loadDatabase():
-    return 0
+    pass
     #return sqlite3.connect("database.db")
 
-def __main__():
+def __main__() -> int:
     loginInfo = accounts.getLoginInfo()
+
+    if len(loginInfo['users']) == 0:
+        #create user example
+        createUser(loginInfo, 'coolUser777', 'password123')
+    else:
+        pass
+        #login
+        #connection = loadDatabase()       
+
     accounts.saveLoginInfo(loginInfo)
-    #connection = loadDatabase()
+   
     return 0
 
 __main__()
