7416582cd733ae564cfe3959a71389deb2c1f827
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-08-27 05:43:47 -0400

    Added saving-and-retriving data methods to the DatabaseConnection class

--- Password_Manager/src/database.py
+++ Password_Manager/src/database.py
@@ -1,8 +1,11 @@
 import sqlite3
+from cryptography.fernet import Fernet
 
 class DatabaseConnection:
     
     _username = None
+    _encryptionKey = None
+    _fernet = None
     _connection = None
     _cursor = None
     _databasePath = None
@@ -12,15 +15,30 @@ class DatabaseConnection:
         self._username = username
         self._connection = sqlite3.connect(self._folderPath + username + ".db")
         self._cursor = self._connection.cursor()
-        self.createTable()
-        
-    def createTable(self) -> None:
-        if (self._cursor.execute(r"SELECT name FROM sqlite_master WHERE name='Passwords'").fetchone() is None):
-            self._cursor.execute("CREATE TABLE Passwords(Password)")
-        else:
-            pass
+        self._encryptionKey = password
+        encryptionKeyBytes = password.encode().ljust(32)[:32]
+        fernetKey = base64.urlsafe_b64encode(encryptionKeyBytes)
+        self._fernet = Fernet(fernetKey)
+        if not self._doesPasswordTableExist():
+            self._createTable()
+    
+    def _doesPasswordTableExist(self) -> bool:
+        return self._cursor.execute(r"SELECT name FROM sqlite_master WHERE name='Passwords'").fetchone() is not None
 
-    def save(password: str) -> None:
-        pass
+    def _createTable(self) -> None:
+        self._cursor.execute("CREATE TABLE Passwords(Key, Password)")
 
-        
+    def doesKeyExist(self, key) -> bool:
+        return self._cursor.execute(r"SELECT * FROM Passwords WHERE key='?'", key).fetchone() is not None
+
+    def savePassword(self, key: str, password: str) -> None:
+        data = (key, self._fernet.encrypt(password.encode()))
+        self._cursor.execute("INSERT INTO Passwords VALUES(?, ?)", data)
+        self._connection.commit()
+
+    def getSavedPasswords(self) -> list:
+        passwords = self._cursor.execute("SELECT * FROM Passwords").fetchall()
+        for key, password in passwords:
+            password = self._fernet.decrypt(password.encode())
+        return passwords
+
