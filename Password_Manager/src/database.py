608561d06d572df4c738fe66e1c4eb2e578ea9eb
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-08-27 19:28:51 -0400

    modified the constructor of the DataConnection class to load database on instantiation

--- Password_Manager/src/database.py
+++ Password_Manager/src/database.py
@@ -1,32 +1,30 @@
 import sqlite3
+import base64
 from cryptography.fernet import Fernet
 
 class DatabaseConnection:
     
-    _username = None
-    _encryptionKey = None
-    _fernet = None
-    _connection = None
-    _cursor = None
-    _databasePath = None
-    _folderPath = r"../users/"
-
-    def load(self, username: str, password: str) -> None:
-        self._username = username
-        self._connection = sqlite3.connect(self._folderPath + username + ".db")
+    def __init__(self, username: str, password: str) -> None:
+        self._initConnection(username)
+        self._initFernet(password)
+    
+    def _initConnection(self, username: str) -> None:
+        self._connection = sqlite3.connect(r"../users/" + username + ".db")
         self._cursor = self._connection.cursor()
-        self._encryptionKey = password
+        if not self._doesPasswordTableExist():
+            self._createTable()
+
+    def _initFernet(self, password: str) -> None:
+        encryptionKey = password
         encryptionKeyBytes = password.encode().ljust(32)[:32]
         fernetKey = base64.urlsafe_b64encode(encryptionKeyBytes)
         self._fernet = Fernet(fernetKey)
-        if not self._doesPasswordTableExist():
-            self._createTable()
-    
+
     def _doesPasswordTableExist(self) -> bool:
         return self._cursor.execute(r"SELECT name FROM sqlite_master WHERE name='Passwords'").fetchone() is not None
 
     def _createTable(self) -> None:
-        self._cursor.execute("CREATE TABLE Passwords(Key, Password)")
+        self._cursor.execute("CREATE TABLE Passwords(key, password)")
 
     def doesKeyExist(self, key) -> bool:
         return self._cursor.execute(r"SELECT * FROM Passwords WHERE key='?'", key).fetchone() is not None
@@ -42,3 +40,7 @@ class DatabaseConnection:
             password = self._fernet.decrypt(password.encode())
         return passwords
 
+    def updatePassword(self, key: str, password: str) -> None:
+        data = (key, self._fernet.encrypt(password.encode()))
+        self._cursor.execute(r"UPDATE Passwords SET key='?', password='?'", data)
+        self._connection.commit()
